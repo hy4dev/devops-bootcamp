0f30baf13682c6069f9bdd5ba7bdb5d18593d265
hy4dev <hafifi.yahya@outlook.com>
2026-08-08 20:26:45 +0800

    Change for Lab 8

--- terraform3/ec2.tf
+++ terraform3/ec2.tf
@@ -12,6 +12,10 @@ data "aws_iam_instance_profile" "my_ssm_profile" {
   name = "EC2-SSM-Role"
 }
 
+data "aws_ssm_parameter" "token" {
+  name = "/devops-bootcamp-2026/tunnel-token"
+}
+
 module "my_server_public" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 6.0"
@@ -26,4 +30,20 @@ module "my_server_public" {
 
   user_data = templatefile("userdata.sh", {})
   tags      = { Name = "tf-server-public" }
+}
+
+module "my_server_private" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+
+  name                 = "tf-server-private"
+  ami                  = data.aws_ami.my_ami.id
+  instance_type        = "t3.micro"
+  subnet_id            = module.my_vpc.private_subnets[0]
+  iam_instance_profile = data.aws_iam_instance_profile.my_ssm_profile.name
+
+  user_data = templatefile("userdata-tunnel.sh", {
+    tunnel_token = data.aws_ssm_parameter.token.value
+  })
+  tags = { Name = "tf-server-private" }
 }
--- terraform3/outputs.tf
+++ terraform3/outputs.tf
@@ -5,3 +5,11 @@ output "server_ip_public" {
 output "ssm_command_public" {
   value = "aws ssm start-session --target ${module.my_server_public.id}"
 }
+
+output "server_ip_private" {
+  value = module.my_server_private.private_ip
+}
+
+output "ssm_command_private" {
+  value = "aws ssm start-session --target ${module.my_server_private.id}"
+}
